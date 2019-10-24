d946f827f8351097bebca22a4ec6285bc5603cb1
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-10-24 11:24:21 +0530

    displayed whole data with loop

--- javascript/fun.js
+++ javascript/fun.js
@@ -2,6 +2,7 @@ let storeAddress;
 let storeMobileno;
 let storeUsername;
 let jsondata;
+let currentCount=0;
 $(document).ready(function(){
     $.ajax({
         type:"GET",
@@ -10,14 +11,23 @@ $(document).ready(function(){
  }).done(function(data){
      jsondata = data;
      customerlist(jsondata);
+     $('.completeinfo').hide();
      $('.customerinfo').click(function(){
-         customerlist(jsondata.username);
-         $('.displayinfo').text(jsondata.username);
+         customerlist(jsondata);  
      })
  })
 })
 function customerlist(jsondata){
     console.log(jsondata);
+    $('.completeinfo').show();
+    if(currentCount < 6){
+    $('.displayinfo-1').text(jsondata[currentCount].username);
+    $('.displayinfo-2').text(jsondata[currentCount].address);
+    $('.displayinfo-3').text(jsondata[currentCount].mobileno);
+    
+    console.log(currentCount);
+    }
+    currentCount++;
 }
 $('.newUser').hide();
 $('.list').hide();
