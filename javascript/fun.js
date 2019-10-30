31d86b9979ea962c6288026030e7386bffc9d007
Robin Hooda <robinhooda66@gmail.com>
2019-10-30 15:20:18 +0530

    fetching data

--- javascript/fun.js
+++ javascript/fun.js
@@ -1,12 +1,69 @@
 let storeAddress;
 let storeMobileno;
 let storeUsername;
+let jsondata;
+let currentCount=0;
+let store;
+let results = "";
+let data;
+// let compareusername;
+// let storeQuantity;
+$(document).ready(function(){
+    $.ajax({
+        type:"GET",
+        dataType:"json",
+        url:"http://localhost:2103/",
+ }).done(function(data){
+     console.log(data);
+     jsondata = data;
+     $('.customerbtn').click(function(){
+         customerlist(jsondata);  
+     })
+ })
+})
+function customerlist(jsondata){
+    console.log(jsondata);
+    //Displaying all the customer list
+    jsondata.forEach(data => {
+      results += `
+      
+        <ul class="list-group mb-4 shadow-sm">
+          <li class="list-group-item bg-dark text-light font-weight-bold">Customer</li>
+          <li class="list-group-item">Company: ${data.username}</li>
+          <li class="list-group-item">Address: ${data.address}</li>
+          <li class="list-group-item">MobileNo: ${data.mobileno}</li>
+        </ul>
+      `;
+    });
+    // document.getElementById("output").innerHTML = results;
+    $('.displayinfo').html(results);
+}
 $('.newUser').hide();
 $('.list').hide();
 $('.customerbtn').click(function(){
     $('.customer,.report').hide();
     $('.newUser,.list').show();
 });
+// $('.addamount').click(function(){
+//     compareusername = $('.matchusername').val();
+//     storeQuantity = $('.quantity').val();
+//     // console.log(compareusername);
+//     jsondata.find(compare=>{
+//         // console.log(compare);
+//         if(compareusername == compare.username ){
+//             // console.log("hello");
+//             $.ajax({
+//                 type:'PUT',
+//                 dataType:'json',
+//                 url:'http://localhost:2103/',
+//                 data:{
+//                     'quantity':storeQuantity
+//                 }
+//             })
+//         }
+//     })
+    
+// })
 $('.registerbtn').click(function(){
     storeUsername = $('.authusername').val();
     console.log(storeUsername);
@@ -23,10 +80,5 @@ $('.registerbtn').click(function(){
             'address':storeAddress,
             'mobileno':storeMobileno 
         },
-        //    success:function(data) {
-        //    main = data;
-        //     getQues(main);
-        // }
     });
 })
-
