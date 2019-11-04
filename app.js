b416ca199fad6dbd18223e43b1546d3c6f747a62
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-11-04 18:40:18 +0530

    port chnaged in app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://pratiksha:rBBM2fscP3UQnk4H@cluster0-8sqx5.mongodb.net/admin?retryWrites=true&w=majority";
 const client = new MongoClient(uri,{ useUnifiedTopology: true }, { useNewUrlParser: true });
 const app = express();
-const port = 2103;
+const port = 3217;
 const cors=require('cors');
 const body=require('body-parser');
 client.connect(err => {
@@ -15,6 +15,6 @@ app.use(cors());
 app.use(body());
 require('./javascript/route')(app, col);
 app.listen(port, () => {
-console.log("Port 2103 running on browser...");
+console.log("Port 3217 running on browser...");
 });
 client.close()});
