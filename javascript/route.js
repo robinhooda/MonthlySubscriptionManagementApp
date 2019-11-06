cd7367c25f3b4ad24eaf8e41fa9991faeb02d526
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-11-06 10:09:30 +0530

    using mongoose updated route

--- javascript/route.js
+++ javascript/route.js
@@ -1,48 +1,63 @@
 const express = require('express');
 const router = express.Router();
 const Store =require("../Model/upload");
+const Quantity = require("../Model/quantity");
+    
+router.post("/alldata", (req, res) => {
 
-    router.post("/alldata", (req, res) => {
-        // console.log('/');
-        // const store={username:req.body.username, address:req.body.address, mobileno:req.body.mobileno};
-        const note = req.body;
-        console.log(note);
-            const newStore = new Store({
-                username:note.username,
-                address:note.address,
-                mobileno:note.mobileno
-            })
-            newStore.save().then(cust=>{
-                if(!cust)
-                return res.status(404).json({"error":"user notstored"})
-                console.log(user);
-                res.status(200).json({"success":"user stored successfully"})
-              })
-              .catch(err => console.log(err)); 
+    const note={username:req.body.username, address:req.body.address, mobileno:req.body.mobileno};
+    
+    console.log(note);
+    const newStore = new Store({
+        username:note.username,
+        address:note.address,
+        mobileno:note.mobileno
+    })
+    
+    newStore.save().then(userdata=>{
+        if(!userdata)
+            return res.status(404).json({"error":"user notstored"})
+            console.log(userdata);
+            res.status(200).json({"success":"user stored successfully"})
+    })
+    .catch(err => console.log(err)); 
                 
-            })
+});
             // col.collection("information").insertOne(store, function (err, result) {
             //     console.log("successful");
             // if (err) throw err;
-    // app.post("/Quantityperday",(req,res)=>{
-    //     console.log(req.body);
-    //     const storedata= {username:req.body.username, date:req.body.date, quantity:req.body.quantity};
-    //     col.collection("Quantityperday").insertOne(storedata, function (err, result) {
-    //         console.log("quantity is added");
-    //     if (err) throw err;
-    // });
-    // })
-   
-    router.get("/alldata", (req, res) => {
-        Store.find({}).toArray(function(err, document) {
-            // console.log(document);
-            res.send(document);
-          });
+router.post("/Quantityperday",(req,res)=>{
+    console.log("here");
+    const storedata= {username:req.body.username, date:req.body.date, quantity:req.body.quantity};
+    
+    const newQuantity = new Quantity({
+        username:storedata.username,
+        date:storedata.date,
+        quantity:storedata.quantity
     });
-    // app.get("/Quantityperday", (req, res) => {
-    //     col.collection('Quantityperday').find({}).toArray(function(err, document) {
-    //         // console.log(document); 
-    //         res.send(document);
-    //       });
-    // });
- module.exports = router;
+
+    newQuantity.save().then(quant=>{
+        if(!quant)
+        return res.status(404).json({"error":"Quantity is not stored"})
+        console.log(quant);
+        res.status(200).json({"success":"Quantity stored successfully"})
+    })
+    .catch(err => console.log(err));
+});
+
+   
+router.get("/alldata", (req, res) => {
+    Store.find()
+        .then((h)=>{
+            res.json({"res":h})
+        })
+});
+router.get("/Quantityperday", (req, res) => {
+    Quantity.find()
+        .then((d)=>{
+            console.log(d)
+            res.json({"res":d});
+        });
+});
+ 
+module.exports = router;
