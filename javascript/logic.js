8c2b0c1fb57886d12378c2f9142726ce4a1a393a
Pratikshatippe <54614091+Pratikshatippe@users.noreply.github.com>
2019-11-11 10:46:49 +0530

    single use quantity

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,13 +1,24 @@
-let totalQuantity=0;
+let totalQuantity = 0;
+let userTotalQuantity = 0;
 module.exports ={
     findQuantity : function(document){
-    console.log(document);
-    document.forEach(element => {
-        console.log(element.quantity);
-        totalQuantity = totalQuantity + element.quantity;
-    });
-    return totalQuantity;
-}
+        console.log(document);
+        document.forEach(element => {
+            // console.log(element.quantity);
+            totalQuantity = totalQuantity + element.quantity;
+        });
+        return totalQuantity;
+    },
+    findUserQuantity : function(document){
+        document.forEach(element=>{
+            console.log(element.username);
+            if(element.username == element.username){
+                userTotalQuantity = userTotalQuantity + element.quantity;
+            }
+        });
+        console.log(userTotalQuantity);
+        return userTotalQuantity;
+    }
 };
 
 
